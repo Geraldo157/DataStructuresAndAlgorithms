42c13bca3382f2f36dcecd13e5f37bd20d5f98ac
Geraldo157 <gsmecanica76@gmail.com>
2023-04-23 01:57:37 -0300

    Binary search more random

--- binary_search.py
+++ binary_search.py
@@ -1,16 +1,29 @@
+import random
+
 valid = True
-lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]
-num = int(input())
+lista = []
+
+for i in range(100):
+    ran = round(random.randint(0, 1000))
+    if ran not in lista:
+        lista.append(ran)
+
 while valid:
-    mid = len(lista) // 2
-    if num < lista[mid]:
-        lista = lista[0:mid]
-        print(lista)
-    elif num > lista[mid]:
-        lista = lista[mid:-1]
-        print(lista)
-    elif num == lista[mid]:
-        lista = lista[mid]
-        valid = False
+    num = int(input('Type a number to be found'))
+    if num in lista:
+        lista.sort()
+        while valid:
+            mid = len(lista) // 2
+            if num < lista[mid]:
+                lista = lista[0:mid]
+                print(lista)
+            elif num > lista[mid]:
+                lista = lista[mid:-1]
+                print(lista)
+            elif num == lista[mid]:
+                lista = lista[mid]
+                valid = False
+    else:
+        print('This number not in the list, try again')
 
 print(lista)
